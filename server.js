0683fca9cb4fd27f03c8b812a179eff61c110ee2
tikatika1234 <ikegoike1130@gmail.com>
2025-08-01 17:46:54 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 'public'フォルダ内の静的ファイルを提供
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
