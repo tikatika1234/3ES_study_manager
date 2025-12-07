5321bfb4d4b1ad125e3c174e948db5988f628436
tikatika1234 <ikegoike1130@gmail.com>
2025-12-07 11:03:08 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 // ...existing code...
-const API_URL = 'https://threees-study-manager.onrender.com';
+const API_URL = 'http://160.251.251.133/';
 
 document.addEventListener('DOMContentLoaded', () => {
     const userData = JSON.parse(localStorage.getItem('userData'));
