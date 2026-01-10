630933d297ce26eed37425354e5ddf959a3e5844
tikatika1234 <ikegoike1130@gmail.com>
2026-01-10 11:39:27 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
-// ...existing code...
-const API_URL = 'http://160.251.251.133:3000';
+const API_URL = 'http://163.44.113.224:3000';
 
 document.addEventListener('DOMContentLoaded', () => {
     const userData = JSON.parse(localStorage.getItem('userData'));
@@ -116,4 +115,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // 初期表示
     loadLatestRecord();
 });
-// ...existing code...
